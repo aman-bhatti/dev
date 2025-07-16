010db829f20eef62cab1f9048c957817a1d9bf57
aman <aibhatti001@gmail.com>
2025-07-16 02:36:39 -0400

    whatever

--- env/.config/nvim/lua/aib/plugins/barbar.lua
+++ env/.config/nvim/lua/aib/plugins/barbar.lua
@@ -9,60 +9,117 @@ return {
 			vim.g.barbar_auto_setup = false
 		end,
 		opts = {
-			animation = true,
-			auto_hide = false,
+			animation = false,
+
+			-- Enable/disable current/total tabpages indicator (top right corner)
 			tabpages = true,
-			clickable = true,
-			exclude_ft = { "javascript" },
-			exclude_name = { "package.json" },
+
+			-- A buffer to this direction will be focused (if it exists) when closing the current buffer.
+			-- Valid options are 'left' (the default), 'previous', and 'right'
 			focus_on_close = "left",
-			hide = { extensions = true, inactive = false },
-			highlight_alternate = false,
-			highlight_inactive_file_icons = false,
-			highlight_visible = true,
+
+			-- Hide inactive buffers and file extensions. Other options are `alternate`, `current`, and `visible`.
+			hide = { extensions = false, inactive = false },
+
 			icons = {
-				buffer_index = true,
+				buffer_index = false,
 				buffer_number = false,
 				button = "",
 				diagnostics = {
-					[vim.diagnostic.severity.ERROR] = { enabled = true, icon = " " },
-					[vim.diagnostic.severity.WARN] = { enabled = false },
-					[vim.diagnostic.severity.INFO] = { enabled = false },
-					[vim.diagnostic.severity.HINT] = { enabled = true },
+					[vim.diagnostic.severity.ERROR] = { enabled = true, icon = " " },
 				},
 				gitsigns = {
-					added = { enabled = true, icon = "+" },
-					changed = { enabled = true, icon = "~" },
-					deleted = { enabled = true, icon = "-" },
+					added = { enabled = true, icon = " " },
+					changed = { enabled = true, icon = " " },
+					deleted = { enabled = true, icon = " " },
 				},
-				filetype = { custom_colors = false, enabled = true },
-				separator = { left = "| ", right = "" },
+				separator = { left = "▎", right = "" },
+
+				-- If true, add an additional separator at the end of the buffer list
 				separator_at_end = true,
+
+				-- Configure the icons on the bufferline when modified or pinned.
+				-- Supports all the base icon options.
 				modified = { button = "●" },
 				pinned = { button = "", filename = true },
-				preset = "default",
+
+				-- Configure the icons on the bufferline based on the visibility of a buffer.
+				-- Supports all the base icon options, plus `modified` and `pinned`.
 				alternate = { filetype = { enabled = false } },
 				current = { buffer_index = true },
 				inactive = { button = "×" },
 				visible = { modified = { buffer_number = false } },
 			},
-			insert_at_end = false,
-			insert_at_start = false,
-			maximum_padding = 1,
-			minimum_padding = 1,
-			maximum_length = 30,
-			minimum_length = 0,
-			semantic_letters = true,
-			sidebar_filetypes = {
+
+			sidebar_filetypes = { -- Set the filetypes which barbar will offset itself for
+				-- Use the default values: {event = 'BufWinLeave', text = '', align = 'left'}
 				NvimTree = true,
-				undotree = { text = "undotree", align = "center" },
+				-- Or, specify the text used for the offset:
+				undotree = {
+					text = "undotree",
+					align = "left", -- *optionally* specify an alignment (either 'left', 'center', or 'right')
+				},
+				-- Or, specify the event which the sidebar executes when leaving:
 				["neo-tree"] = { event = "BufWipeout" },
+				-- Or, specify all three
 				Outline = { event = "BufWinLeave", text = "symbols-outline", align = "right" },
 			},
-			letters = "asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP",
-			no_name_title = nil,
-			sort = { ignore_case = true },
+			maximum_length = 25, -- Sets the maximum buffer name length.
+			-- 		animation = true,
+			-- 		auto_hide = false,
+			-- 		tabpages = true,
+			-- 		clickable = true,
+			-- 		exclude_ft = { "javascript" },
+			-- 		exclude_name = { "package.json" },
+			-- 		focus_on_close = "left",
+			-- 		hide = { extensions = true, inactive = false },
+			-- 		highlight_alternate = false,
+			-- 		highlight_inactive_file_icons = false,
+			-- 		highlight_visible = true,
+			-- 		icons = {
+			-- 			buffer_index = true,
+			-- 			buffer_number = false,
+			-- 			button = "",
+			-- 			diagnostics = {
+			-- 				[vim.diagnostic.severity.ERROR] = { enabled = true, icon = " " },
+			-- 				[vim.diagnostic.severity.WARN] = { enabled = false },
+			-- 				[vim.diagnostic.severity.INFO] = { enabled = false },
+			-- 				[vim.diagnostic.severity.HINT] = { enabled = true },
+			-- 			},
+			-- 			gitsigns = {
+			-- 				added = { enabled = true, icon = "+" },
+			-- 				changed = { enabled = true, icon = "~" },
+			-- 				deleted = { enabled = true, icon = "-" },
+			-- 			},
+			-- 			filetype = { custom_colors = false, enabled = true },
+			-- 			separator = { left = "| ", right = "" },
+			-- 			separator_at_end = true,
+			-- 			modified = { button = "●" },
+			-- 			pinned = { button = "", filename = true },
+			-- 			preset = "default",
+			-- 			alternate = { filetype = { enabled = false } },
+			-- 			current = { buffer_index = true },
+			-- 			inactive = { button = "×" },
+			-- 			visible = { modified = { buffer_number = false } },
+			-- 		},
+			-- 		insert_at_end = false,
+			-- 		insert_at_start = false,
+			-- 		maximum_padding = 1,
+			-- 		minimum_padding = 1,
+			-- 		maximum_length = 30,
+			-- 		minimum_length = 0,
+			-- 		semantic_letters = true,
+			-- 		sidebar_filetypes = {
+			-- 			NvimTree = true,
+			-- 			undotree = { text = "undotree", align = "center" },
+			-- 			["neo-tree"] = { event = "BufWipeout" },
+			-- 			Outline = { event = "BufWinLeave", text = "symbols-outline", align = "right" },
+			-- 		},
+			-- 		letters = "asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP",
+			-- 		no_name_title = nil,
+			-- 		sort = { ignore_case = true },
+			-- 	},
+			-- 	version = "^1.0.0",
 		},
-		version = "^1.0.0",
 	},
 }
--- env/.config/nvim/lua/aib/plugins/render-markdown.lua
+++ env/.config/nvim/lua/aib/plugins/render-markdown.lua
@@ -1,3 +1,115 @@
+-- return {
+-- 	"MeanderingProgrammer/render-markdown.nvim",
+-- 	enabled = true,
+-- 	dependencies = {
+-- 		"nvim-treesitter/nvim-treesitter",
+-- 		"nvim-tree/nvim-web-devicons",
+-- 	},
+-- 	opts = {
+-- 		link = {
+-- 			enabled = true,
+-- 			render_modes = false,
+-- 			footnote = {
+-- 				enabled = true,
+-- 				superscript = true,
+-- 				prefix = "",
+-- 				suffix = "",
+-- 			},
+-- 			image = "󰥶 ",
+-- 			email = "󰀓 ",
+-- 			hyperlink = "󰌹 ",
+-- 			highlight = "RenderMarkdownLink",
+-- 			wiki = {
+-- 				icon = "󱗖 ",
+-- 				body = function()
+-- 					return nil
+-- 				end,
+-- 				highlight = "RenderMarkdownWikiLink",
+-- 			},
+-- 			custom = {
+-- 				web = { pattern = "^http", icon = "󰖟 " },
+-- 				github = { pattern = "github%.com", icon = "󰊤 " },
+-- 				gitlab = { pattern = "gitlab%.com", icon = "󰮠 " },
+-- 				stackoverflow = { pattern = "stackoverflow%.com", icon = "󰓌 " },
+-- 				wikipedia = { pattern = "wikipedia%.org", icon = "󰖬 " },
+-- 				youtube = { pattern = "youtube%.com", icon = "󰗃 " },
+-- 			},
+-- 		},
+-- 		callout = {
+-- 			note = { raw = "[!NOTE]", rendered = "󰋽 Note", highlight = "RenderMarkdownInfo" },
+-- 			tip = { raw = "[!TIP]", rendered = "󰌶 Tip", highlight = "RenderMarkdownSuccess" },
+-- 			important = { raw = "[!IMPORTANT]", rendered = "󰅾 Important", highlight = "RenderMarkdownHint" },
+-- 			warning = { raw = "[!WARNING]", rendered = "󰀪 Warning", highlight = "RenderMarkdownWarn" },
+-- 			caution = { raw = "[!CAUTION]", rendered = "󰳦 Caution", highlight = "RenderMarkdownError" },
+-- 			abstract = { raw = "[!ABSTRACT]", rendered = "󰨸 Abstract", highlight = "RenderMarkdownInfo" },
+-- 			summary = { raw = "[!SUMMARY]", rendered = "󰨸 Summary", highlight = "RenderMarkdownInfo" },
+-- 			tldr = { raw = "[!TLDR]", rendered = "󰨸 Tldr", highlight = "RenderMarkdownInfo" },
+-- 			info = { raw = "[!INFO]", rendered = "󰋽 Info", highlight = "RenderMarkdownInfo" },
+-- 			todo = { raw = "[!TODO]", rendered = "󰗡 Todo", highlight = "RenderMarkdownInfo" },
+-- 			hint = { raw = "[!HINT]", rendered = "󰌶 Hint", highlight = "RenderMarkdownSuccess" },
+-- 			success = { raw = "[!SUCCESS]", rendered = "󰄬 Success", highlight = "RenderMarkdownSuccess" },
+-- 			check = { raw = "[!CHECK]", rendered = "󰄬 Check", highlight = "RenderMarkdownSuccess" },
+-- 			done = { raw = "[!DONE]", rendered = "󰄬 Done", highlight = "RenderMarkdownSuccess" },
+-- 			question = { raw = "[!QUESTION]", rendered = "󰘥 Question", highlight = "RenderMarkdownWarn" },
+-- 			help = { raw = "[!HELP]", rendered = "󰘥 Help", highlight = "RenderMarkdownWarn" },
+-- 			faq = { raw = "[!FAQ]", rendered = "󰘥 Faq", highlight = "RenderMarkdownWarn" },
+-- 			attention = { raw = "[!ATTENTION]", rendered = "󰀪 Attention", highlight = "RenderMarkdownWarn" },
+-- 			failure = { raw = "[!FAILURE]", rendered = "󰅖 Failure", highlight = "RenderMarkdownError" },
+-- 			fail = { raw = "[!FAIL]", rendered = "󰅖 Fail", highlight = "RenderMarkdownError" },
+-- 			missing = { raw = "[!MISSING]", rendered = "󰅖 Missing", highlight = "RenderMarkdownError" },
+-- 			danger = { raw = "[!DANGER]", rendered = "󱐌 Danger", highlight = "RenderMarkdownError" },
+-- 			error = { raw = "[!ERROR]", rendered = "󱐌 Error", highlight = "RenderMarkdownError" },
+-- 			bug = { raw = "[!BUG]", rendered = "󰨰 Bug", highlight = "RenderMarkdownError" },
+-- 			example = { raw = "[!EXAMPLE]", rendered = "󰉹 Example", highlight = "RenderMarkdownHint" },
+-- 			quote = { raw = "[!QUOTE]", rendered = "󱆨 Quote", highlight = "RenderMarkdownQuote" },
+-- 			cite = { raw = "[!CITE]", rendered = "󱆨 Cite", highlight = "RenderMarkdownQuote" },
+-- 		},
+-- 		checkbox = {
+-- 			enabled = true,
+-- 			render_modes = false,
+-- 			bullet = false,
+-- 			right_pad = 1,
+-- 			unchecked = {
+-- 				icon = "󰄱 ",
+-- 				highlight = "RenderMarkdownUnchecked",
+-- 				scope_highlight = nil,
+-- 			},
+-- 			checked = {
+-- 				icon = "󰱒 ",
+-- 				highlight = "RenderMarkdownChecked",
+-- 				scope_highlight = nil,
+-- 			},
+-- 			custom = {
+-- 				todo = { raw = "[-]", rendered = "󰥔 ", highlight = "RenderMarkdownTodo", scope_highlight = nil },
+-- 			},
+-- 		},
+-- 		bullet = {
+-- 			enabled = true,
+-- 			render_modes = false,
+-- 			icons = { "●", "○", "◆", "◇" },
+-- 			ordered_icons = function(ctx)
+-- 				local value = vim.trim(ctx.value)
+-- 				local index = tonumber(value:sub(1, #value - 1))
+-- 				return ("%d."):format(index > 1 and index or ctx.index)
+-- 			end,
+-- 			left_pad = 0,
+-- 			right_pad = 0,
+-- 			highlight = "RenderMarkdownBullet",
+-- 			scope_highlight = {},
+-- 		},
+-- 		quote = { icon = "▋" },
+-- 		anti_conceal = {
+-- 			enabled = true,
+-- 			ignore = {
+-- 				code_background = true,
+-- 				sign = true,
+-- 			},
+-- 			above = 0,
+-- 			below = 0,
+-- 		},
+-- 	},
+-- }
+
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
 	enabled = true,
@@ -62,29 +174,29 @@ return {
 			-- Turn on / off list bullet rendering
 			enabled = true,
 		},
-		-- checkbox = {
-		--     -- Turn on / off checkbox state rendering
-		--     enabled = true,
-		--     -- Determines how icons fill the available space:
-		--     --  inline:  underlying text is concealed resulting in a left aligned icon
-		--     --  overlay: result is left padded with spaces to hide any additional text
-		--     position = "inline",
-		--     unchecked = {
-		--         -- Replaces '[ ]' of 'task_list_marker_unchecked'
-		--         icon = "   󰄱 ",
-		--         -- Highlight for the unchecked icon
-		--         highlight = "RenderMarkdownUnchecked",
-		--         -- Highlight for item associated with unchecked checkbox
-		--         scope_highlight = nil,
-		--     },
-		--     checked = {
-		--         -- Replaces '[x]' of 'task_list_marker_checked'
-		--         icon = "   󰱒 ",
-		--         -- Highlight for the checked icon
-		--         highlight = "RenderMarkdownChecked",
-		--         -- Highlight for item associated with checked checkbox
-		--         scope_highlight = nil,
-		--     },
-		-- },
+		checkbox = {
+			-- Turn on / off checkbox state rendering
+			enabled = true,
+			-- Determines how icons fill the available space:
+			--  inline:  underlying text is concealed resulting in a left aligned icon
+			--  overlay: result is left padded with spaces to hide any additional text
+			position = "inline",
+			unchecked = {
+				-- Replaces '[ ]' of 'task_list_marker_unchecked'
+				icon = "   󰄱 ",
+				-- Highlight for the unchecked icon
+				highlight = "RenderMarkdownUnchecked",
+				-- Highlight for item associated with unchecked checkbox
+				scope_highlight = nil,
+			},
+			checked = {
+				-- Replaces '[x]' of 'task_list_marker_checked'
+				icon = "   󰱒 ",
+				-- Highlight for the checked icon
+				highlight = "RenderMarkdownChecked",
+				-- Highlight for item associated with checked checkbox
+				scope_highlight = nil,
+			},
+		},
 	},
 }
