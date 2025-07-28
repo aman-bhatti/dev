9bd73a445ae7b33249acc517344ec56349c8d238
aman <aibhatti001@gmail.com>
2025-07-28 02:21:12 -0400

    testing pull icon

--- env/.config/doom/config.el
+++ env/.config/doom/config.el
@@ -81,7 +81,7 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
-(setq fancy-splash-image "/home/aman/.config/doom/image.jpg")
+(setq fancy-splash-image "/home/aman/.config/doom/image(1).jpg")
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
