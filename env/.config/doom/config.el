7eb32b13279294d8d787ee627eb45a7e7ce42f8d
aman <aibhatti001@gmail.com>
2025-07-31 00:08:46 -0400

    fixing org ui mode

--- env/.config/doom/config.el
+++ env/.config/doom/config.el
@@ -60,6 +60,23 @@
   :config
   (setq org-modern-tag 'inherit))
 
+(use-package! websocket
+    :after org-roam)
+
+;; ========== ORG-ROAM-UI ==========
+(use-package! org-roam-ui
+    :after org-roam ;; or :after org
+;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+;;         a hookable mode anymore, you're advised to pick something yourself
+;;         if you don't care about startup time, use
+;;  :hook (after-init . org-roam-ui-mode)
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
+
+
 ;; ========== ORG-ROAM KEYBINDINGS ==========
 (map! :leader
       :prefix ("n" . "notes")
