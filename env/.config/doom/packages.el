8655f4c463ee5f77aaab08f99efb0707691a268c
aman <aibhatti001@gmail.com>
2025-08-04 01:54:11 -0400

    updated doom config

--- env/.config/doom/packages.el
+++ env/.config/doom/packages.el
@@ -6,7 +6,10 @@
 (package! org-roam)
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;; (package! some-package)
-
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(package-initialize)
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/radian-software/straight.el#the-recipe-format
@@ -46,3 +49,4 @@
 ;; (unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;; (unpin! t)
+
